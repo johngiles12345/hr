6dc0ba43c08a65ed18ae2739d4baa1b6d3687f1d
jgiles12345 <jgiles12345@gmail.com>
2021-08-11 10:24:03 -0700

    Added Makefile and updated README.rst

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: default install test
+default: test
+install: pipenv install --dev --skip-lock
+test: PYTHONPATH=./src pytest
+
--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ setup(
     version='0.1.0',
     description='Commandline user management utility',
     long_description=readme,
-    author='Your Name',
-    author_email='person@example.com',
-    packages=find_packages('src'),
+    author='John Giles',
+    author_email='johngiles12345@gmail.com',
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'hr=hr.cli:main',
+        ]
+    }
 )
 
